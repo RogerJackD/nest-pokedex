a582199dd3b5338b1cb04f30ba2a874ff96e38e8
RogerJackD <1511638@senati.pe>
2025-06-14 00:05:09 -0500

    reference to mongodb with mongoose added

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true, //limpia la data innecesaria del request
+      forbidNonWhitelisted: true, //habilita las reglas establecitas de nuestro DTO
+    })
+  )
+
+  app.setGlobalPrefix('api/v2')
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
