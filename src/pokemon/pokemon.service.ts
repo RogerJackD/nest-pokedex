79f7013516dafaa0129042e2618807e681bf030a
RogerJackD <1511638@senati.pe>
2025-06-14 23:35:55 -0500

    find pokemon by mongoId, no or name, added

--- src/pokemon/pokemon.service.ts
+++ src/pokemon/pokemon.service.ts
@@ -1,20 +1,64 @@
-import { Injectable } from '@nestjs/common';
+import { BadRequestException, Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { isValidObjectId, Model } from 'mongoose';
+import { Pokemon } from './entities/pokemon.entity';
+
 import { CreatePokemonDto } from './dto/create-pokemon.dto';
 import { UpdatePokemonDto } from './dto/update-pokemon.dto';
 
 @Injectable()
 export class PokemonService {
-  create(createPokemonDto: CreatePokemonDto) {
+  
+  constructor(
+    @InjectModel( Pokemon.name )
+    private readonly pokemonModel: Model<Pokemon>
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
     createPokemonDto.name = createPokemonDto.name.toLowerCase();
-    return createPokemonDto;
+
+    try {
+      const pokemon = await this.pokemonModel.create( createPokemonDto )
+      return pokemon;
+      
+    } catch (error) {
+      if( error.code === 11000){
+        throw new BadRequestException(`Pokemon exist in DB ${ JSON.stringify( error.keyValue )}`)
+      }
+      console.log('error is: -' +error)
+      throw new InternalServerErrorException(`Cant create pokemon - check server logs`)
+    }
+
+    
   }
 
   findAll() {
     return `This action returns all pokemon`;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} pokemon`;
+  async findOne(term: string) {
+
+    let pokemon: Pokemon | null = null;
+
+    if( !isNaN(+term) ){
+      pokemon = await this.pokemonModel.findOne({ no: term });
+    }
+
+    //mongoID
+    if ( !pokemon && isValidObjectId( term ) ) {  ///isValidObjectId function that also checks if is mongo id
+      pokemon = await this.pokemonModel.findById( term )
+    }
+    
+    if ( !pokemon ) {
+      pokemon = await this.pokemonModel.findOne({ name : term.toLowerCase().trim() })
+    }
+
+    if( !pokemon ){
+      throw new NotFoundException(`Pokemon with id, name or no ' ${term} ' not found`)
+    }
+
+
+    return pokemon
   }
 
   update(id: number, updatePokemonDto: UpdatePokemonDto) {
