ca1060440a8a17137c032c30cf5783f01e0e46b7
RogerJackD <1511638@senati.pe>
2025-06-23 23:31:50 -0500

    feature: adapter axios added in seed

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,24 +1,25 @@
 import { Injectable } from '@nestjs/common';
-import axios, { AxiosInstance } from 'axios';
 import { PokeResponse } from './interfaces/poke-response.interface';
 import { InjectModel } from '@nestjs/mongoose';
 import { Pokemon } from 'src/pokemon/entities/pokemon.entity';
 import { Model } from 'mongoose';
+import { AxiosAdapter } from 'src/common/adapters/axios.adapter';
 
 @Injectable()
 export class SeedService {
   constructor(
     @InjectModel(Pokemon.name)
     private readonly pokemonModel: Model<Pokemon>,
+
+    private readonly http: AxiosAdapter,
   ) {}
 
-  private readonly axios: AxiosInstance;
 
   async executeSeed() {
 
     await this.pokemonModel.deleteMany({});
 
-    const { data } = await axios.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
+    const data = await this.http.get<PokeResponse>('https://pokeapi.co/api/v2/pokemon?limit=650');
 
     const pokemonToInsert: { no: number, name: string} [] = [];
 
